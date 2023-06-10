7f7ebf6052a430a2653146a268324108103db87e
Courtney Downs <downscourtney17@gmail.com>
2023-06-10 18:45:15 -0400

    Cross step 3 is completed. Also, Added code format for file CubeSolver.java

--- app/src/main/java/com/raifuzu/twistypuzzlemasterz/CubeSolver.java
+++ app/src/main/java/com/raifuzu/twistypuzzlemasterz/CubeSolver.java
@@ -10,11 +10,13 @@ import java.util.Map;
 import android.view.Surface;
 import android.view.View;
 import android.widget.Toast;
+
 import com.raifuzu.twistypuzzlemasterz.RubiksCube.SurfaceName;
 
 
 /**
  * NOTE: This class solves the Rubik's Cube using the CFOP method.
+ *
  * @author downs
  */
 public class CubeSolver {
@@ -22,12 +24,12 @@ public class CubeSolver {
 
     Integer[][] topCubies = {
             {RubiksCube.YELLOW, RubiksCube.BLUE, RubiksCube.ORANGE},
-            {RubiksCube.YELLOW, RubiksCube.BLUE} ,
-            {RubiksCube.YELLOW, RubiksCube.BLUE, RubiksCube.RED} ,
+            {RubiksCube.YELLOW, RubiksCube.BLUE},
+            {RubiksCube.YELLOW, RubiksCube.BLUE, RubiksCube.RED},
             {RubiksCube.YELLOW, RubiksCube.RED},
             {RubiksCube.YELLOW, RubiksCube.RED, RubiksCube.GREEN},
-            {RubiksCube.YELLOW, RubiksCube.GREEN} ,
-            {RubiksCube.YELLOW, RubiksCube.GREEN, RubiksCube.ORANGE} ,
+            {RubiksCube.YELLOW, RubiksCube.GREEN},
+            {RubiksCube.YELLOW, RubiksCube.GREEN, RubiksCube.ORANGE},
             {RubiksCube.YELLOW, RubiksCube.ORANGE}
     };
 
@@ -41,16 +43,16 @@ public class CubeSolver {
     }
 
     /**
-     *   1. Get all pieces on down layer
+     * 1. Get all pieces on down layer
      *
      * @param stickersToSolve Unique stickers list of the specific cubies to solve
      */
-    public void crossStep1(  Integer[][] stickersToSolve){
+    public void crossStep1(Integer[][] stickersToSolve) {
 
-        for(Integer[] cubie : stickersToSolve){
+        for (Integer[] cubie : stickersToSolve) {
 
             // Length of cubieAtList and intersectingLayers will always be 2
-            List<Integer> cubieAsList = Arrays.asList(cubie); 
+            List<Integer> cubieAsList = Arrays.asList(cubie);
             ArrayList<SurfaceName> intersectingLayers = this.rubiksCube.findLocationOfCubie(cubieAsList);
 
             int indexOfU = intersectingLayers.indexOf(SurfaceName.U);
@@ -58,35 +60,34 @@ public class CubeSolver {
             SurfaceName nonUSurface = intersectingLayers.get(indexOfNonU);
 
 
-
             // If cubie on 'up' layer
-            if(intersectingLayers.contains(SurfaceName.U)){
+            if (intersectingLayers.contains(SurfaceName.U)) {
 
                 // Check if there is a white cubie on the D layer directly under U cubie.
                 String[] locationDirectlyBelow = {nonUSurface.name(), SurfaceName.D.name()};
                 CubeLayer.Cubie cubieDirectlyBelow = this.rubiksCube.getCubieAtLocation(locationDirectlyBelow);
 
-                if(cubieDirectlyBelow.getStickerColors().contains(RubiksCube.WHITE)) {
+                if (cubieDirectlyBelow.getStickerColors().contains(RubiksCube.WHITE)) {
 
                     // avoidance maneuver
                     avoidanceManeuverForCross(locationDirectlyBelow);
                 }
 
                 String sideToRotate = nonUSurface.name() + "2";
-                this.rubiksCube.executeAlgorithm( sideToRotate , RubiksCube.RecordAlgorithm.YES);
+                this.rubiksCube.executeAlgorithm(sideToRotate, RubiksCube.RecordAlgorithm.YES);
             }
 
             // If the cubie on 'middle' layer
-            else if( ! intersectingLayers.contains(SurfaceName.D) && ! intersectingLayers.contains(SurfaceName.U) ){
+            else if (!intersectingLayers.contains(SurfaceName.D) && !intersectingLayers.contains(SurfaceName.U)) {
 
                 ArrayList<String> locationAsString = new ArrayList<>();
-                locationAsString.add( intersectingLayers.get(0).name() );
-                locationAsString.add( intersectingLayers.get(1).name() );
-                Collections.sort( locationAsString );
+                locationAsString.add(intersectingLayers.get(0).name());
+                locationAsString.add(intersectingLayers.get(1).name());
+                Collections.sort(locationAsString);
 
                 // Possible locations to insert 'cubie' if it's location is case1
-                String[][] locationsToCheck = { {locationAsString.get(0), "D"},
-                                                {locationAsString.get(1), "D"}   };
+                String[][] locationsToCheck = {{locationAsString.get(0), "D"},
+                        {locationAsString.get(1), "D"}};
 
 
                 // locationsAsString   ->  possibleMoves          {  locationsToCheck }
@@ -97,40 +98,40 @@ public class CubeSolver {
 
                 String keyToPossibleMovesMap = locationsToCheck[0][0] + " " + locationsToCheck[0][1]
                         + " , " + locationsToCheck[1][0] + " " + locationsToCheck[1][1];
-                Map<String , String[]> possibleMovesMap = new HashMap<String , String[]>(){{
-                        put("F D , L D"      , new String[]{"F'" , "L"});
-                        put("B D , L D"      , new String[]{"B"  , "L'"});
-                        put("F D , R D"      , new String[]{"F"  , "R'"});
-                        put("B D , R D"      , new String[]{"B'" , "R"});
+                Map<String, String[]> possibleMovesMap = new HashMap<String, String[]>() {{
+                    put("F D , L D", new String[]{"F'", "L"});
+                    put("B D , L D", new String[]{"B", "L'"});
+                    put("F D , R D", new String[]{"F", "R'"});
+                    put("B D , R D", new String[]{"B'", "R"});
                 }};
 
 
                 List<Boolean> whitePieceExist = Arrays.asList(false, false);
 
-                for(int i = 0; i < locationsToCheck.length; i++){
+                for (int i = 0; i < locationsToCheck.length; i++) {
 
                     String[] checkingLocation = locationsToCheck[i];
 
-                    CubeLayer.Cubie currentCubie1 = this.rubiksCube.getCubieAtLocation( checkingLocation );
+                    CubeLayer.Cubie currentCubie1 = this.rubiksCube.getCubieAtLocation(checkingLocation);
                     ArrayList<Integer> currentCubie = currentCubie1.getStickerColors();
 
                     // Check if a white cubie already exists at the checking location. If yes, do avoidance.
-                    if(currentCubie.contains(RubiksCube.WHITE)){
+                    if (currentCubie.contains(RubiksCube.WHITE)) {
                         whitePieceExist.set(i, true);
                     }
                 }
 
 
                 // Check if both possible moves have white pieces at their locations. If yes, do avoidance.
-                String[] possibleMoves = possibleMovesMap.get( keyToPossibleMovesMap );
-                if( whitePieceExist.get(0) && whitePieceExist.get(1)){
+                String[] possibleMoves = possibleMovesMap.get(keyToPossibleMovesMap);
+                if (whitePieceExist.get(0) && whitePieceExist.get(1)) {
 
-                    this.avoidanceManeuverForCross( locationsToCheck[0] );
+                    this.avoidanceManeuverForCross(locationsToCheck[0]);
                     String moveToExecute = possibleMoves[0];
                     this.rubiksCube.executeAlgorithm(moveToExecute, RubiksCube.RecordAlgorithm.YES);
                 }
                 // Else, perform correct insertion.
-                else{
+                else {
 
                     int indexOfFalse = whitePieceExist.indexOf(false);
                     String moveToExecute = possibleMoves[indexOfFalse];
@@ -143,12 +144,12 @@ public class CubeSolver {
     }
 
 
-    private void avoidanceManeuverForCross(String[] locationDirectlyBelow){
+    private void avoidanceManeuverForCross(String[] locationDirectlyBelow) {
 
         CubeLayer.Cubie cubieDirectlyBelow = this.rubiksCube.getCubieAtLocation(locationDirectlyBelow);
         ArrayList<Integer> stickers = cubieDirectlyBelow.getStickerColors();
 
-        while(stickers.contains(RubiksCube.WHITE) ){
+        while (stickers.contains(RubiksCube.WHITE)) {
 
             // Actually Execute a D rotation
             this.rubiksCube.executeAlgorithm("D", RubiksCube.RecordAlgorithm.YES);
@@ -158,7 +159,7 @@ public class CubeSolver {
         }
     }
 
-    private int crossPiecesInCorrectLocation(){
+    private int crossPiecesInCorrectLocation() {
         String[] location1 = {SurfaceName.F.name(), SurfaceName.D.name()};
         String[] location2 = {SurfaceName.R.name(), SurfaceName.D.name()};
         String[] location3 = {SurfaceName.B.name(), SurfaceName.D.name()};
@@ -166,9 +167,9 @@ public class CubeSolver {
         String[][] locations = {location1, location2, location3, location4};
 
         int numOfCorrectLocations = 0;
-        for(String[] location : locations){
+        for (String[] location : locations) {
             boolean isCorrect = this.rubiksCube.isCorrectCubieAtThisLocation(location);
-            if(isCorrect){
+            if (isCorrect) {
                 numOfCorrectLocations++;
             }
         }
@@ -176,10 +177,10 @@ public class CubeSolver {
     }
 
 
-    public void crossStep2(){
+    public void crossStep2() {
 
         int numOfCorrectLocations = crossPiecesInCorrectLocation();
-        while(numOfCorrectLocations < 2){
+        while (numOfCorrectLocations < 2) {
 
             this.rubiksCube.executeAlgorithm("D", RubiksCube.RecordAlgorithm.YES);
             numOfCorrectLocations = crossPiecesInCorrectLocation();
@@ -187,18 +188,54 @@ public class CubeSolver {
     }
 
 
+    private void cubieOrientationCorrection(CubeLayer.Cubie currentCubie) {
+        if(currentCubie.getLocation().contains(SurfaceName.R.name())){
+            this.rubiksCube.executeAlgorithm("R2 U F R' F'", RubiksCube.RecordAlgorithm.YES);
+        }else if(currentCubie.getLocation().contains(SurfaceName.F.name())){
+            this.rubiksCube.executeAlgorithm("F2 U L F' L'", RubiksCube.RecordAlgorithm.YES);
+        }else if(currentCubie.getLocation().contains(SurfaceName.L.name())){
+            this.rubiksCube.executeAlgorithm("L2 U B L' B'", RubiksCube.RecordAlgorithm.YES);
+        }else if(currentCubie.getLocation().contains(SurfaceName.B.name())){
+            this.rubiksCube.executeAlgorithm("B2 U R B' R'", RubiksCube.RecordAlgorithm.YES);
+        }
+    }
 
 
-    public void crossStep3(){
+    public void crossStep3() {
+
+//        String result = "";
+
+        // Check the 4 cross pieces to see if their orientations are correct.
+        // If a piece is not correct, then use the orientation correction algorithm.
+        String[] location1 = new String[]{"F", "D"};
+        String[] location2 = new String[]{"R", "D"};
+        String[] location3 = new String[]{"B", "D"};
+        String[] location4 = new String[]{"L", "D"};
+        String[][] locationsOfCrossPieces = {location1, location2, location3, location4};
+
+        for (String[] location : locationsOfCrossPieces) {
+            CubeLayer.Cubie crossCubie = this.rubiksCube.getCubieAtLocation(location);
+            Map<String, String> orientationOfCubie = crossCubie.getCubieOrientation();
+
+//            result += orientationOfCubie.toString();
+            // If the white sticker on this cubie is not on the white side.
+            if( ! orientationOfCubie.get("WHITE").equals("WHITE") ){
+                cubieOrientationCorrection(crossCubie);
+            }
+        }
+
+
+//        Toast.makeText(rootView.getContext(), "Cross Orientations: " + result, Toast.LENGTH_LONG).show();
+
 
     }
 
-    public void crossStep4(){
+    public void crossStep4() {
 
     }
 
 
-    public void solveCross(){
+    public void solveCross() {
 
         Integer[][] stickersToSolve = {
                 {RubiksCube.WHITE, RubiksCube.GREEN},
@@ -208,9 +245,9 @@ public class CubeSolver {
         };
 
         // Get all cross pieces on down layer
-        crossStep1(   stickersToSolve  );
+        crossStep1(stickersToSolve);
         // Rotate bottom layer until at least 2 pieces are in correct locations
-       crossStep2(); 
+        crossStep2();
         //Set the cubies that are in correct locations to their correct orientations
         crossStep3();
 
@@ -219,56 +256,52 @@ public class CubeSolver {
         crossStep4();
 
 
-
 //        Toast.makeText(rootView.getContext(), "Solution: " + this.rubiksCube.getSolutionAlgorithm(), Toast.LENGTH_LONG).show();
     }
 
 
-    public void f2lSolutionSteps(  Integer[][][] stickersToSolve){
+    public void f2lSolutionSteps(Integer[][][] stickersToSolve) {
         // TODO: Complete solution implementation
     }
 
-    public void solveF2L(){
+    public void solveF2L() {
         Integer[][][] stickersToSolve = {
-                {{RubiksCube.WHITE, RubiksCube.BLUE, RubiksCube.ORANGE}, { RubiksCube.BLUE, RubiksCube.ORANGE}},
-                {{RubiksCube.WHITE, RubiksCube.BLUE, RubiksCube.RED}, { RubiksCube.BLUE, RubiksCube.RED}},
-                {{RubiksCube.WHITE, RubiksCube.RED, RubiksCube.GREEN}, { RubiksCube.RED, RubiksCube.GREEN}},
-                {{RubiksCube.WHITE, RubiksCube.GREEN, RubiksCube.ORANGE}, { RubiksCube.GREEN, RubiksCube.ORANGE}},
+                {{RubiksCube.WHITE, RubiksCube.BLUE, RubiksCube.ORANGE}, {RubiksCube.BLUE, RubiksCube.ORANGE}},
+                {{RubiksCube.WHITE, RubiksCube.BLUE, RubiksCube.RED}, {RubiksCube.BLUE, RubiksCube.RED}},
+                {{RubiksCube.WHITE, RubiksCube.RED, RubiksCube.GREEN}, {RubiksCube.RED, RubiksCube.GREEN}},
+                {{RubiksCube.WHITE, RubiksCube.GREEN, RubiksCube.ORANGE}, {RubiksCube.GREEN, RubiksCube.ORANGE}},
         };
 
-        f2lSolutionSteps( stickersToSolve );
+        f2lSolutionSteps(stickersToSolve);
     }
 
-    public void ollSolutionSteps(  Integer[][] stickersToSolve){
+    public void ollSolutionSteps(Integer[][] stickersToSolve) {
         // TODO: Complete solution implementation
     }
 
-    public void solveOLL(){
-        ollSolutionSteps( topCubies );
+    public void solveOLL() {
+        ollSolutionSteps(topCubies);
     }
 
 
-
-    public void pllSolutionSteps(  Integer[][] stickersToSolve){
+    public void pllSolutionSteps(Integer[][] stickersToSolve) {
         // TODO: Complete solution implementation
     }
 
-    public void solvePLL(){
-        pllSolutionSteps( topCubies );
+    public void solvePLL() {
+        pllSolutionSteps(topCubies);
     }
 
 
-
-
-    public void solveCube(){
+    public void solveCube() {
 
 //        boolean cubeIsValid = rubiksCube.isValidState();
 
 //        if( cubeIsValid ){
-            solveCross();
-            solveF2L();
-            solveOLL();
-            solvePLL();
+        solveCross();
+        solveF2L();
+        solveOLL();
+        solvePLL();
 
 //        }else{
 //            Toast.makeText(rootView.getContext(), "Invalid State! ", Toast.LENGTH_LONG).show();
